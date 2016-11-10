a251f1cdcab3aacef7b809d3ce140cd03c89ad81
Joe Bujok <joe.bujok@puntersouthall.com>
2016-11-10 00:00:18 +0000

    start of living object stats

--- Assets/Selection.cs
+++ Assets/Selection.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Selection : MonoBehaviour {
 
     public bool selected;
     private Renderer renderer;
+    private Slider energyBar;
+    public Energy energy;
+    private Canvas canvas;
 
-// Use this for initialization
+    // Use this for initialization
     void Start () {
         renderer =  GetComponent<Renderer>();
-
+        energy = GetComponent<Energy>();
+        energyBar = GameObject.Find("SelectedObjectCanvas/Panel/EnergyBar").GetComponent<Slider>();
+        canvas = GameObject.Find("SelectedObjectCanvas").GetComponent<Canvas>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (selected) {
+            this.energyBar.value = energy.EnergyLevel;
+        }
 	    
 	}
 
@@ -22,10 +31,12 @@ public class Selection : MonoBehaviour {
         {
             selected = true;
             renderer.material.shader = Shader.Find("Outlined/Silhouetted Bumped Diffuse");
+            canvas.gameObject.SetActive(selected);
         }
         else {
             selected = false;
             renderer.material.shader = Shader.Find("Standard");
+            canvas.gameObject.SetActive(selected);
         }
 
     }
